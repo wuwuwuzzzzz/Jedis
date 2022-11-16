1ba4a07f2eaf1c6ca8449b026f34b6cd304e16a3
wxz <759126349@qq.com>
2022-11-16 10:40:22 +0800

    优化代码

--- src/main/java/org/example/jedis/SemaphoreDemo.java
+++ src/main/java/org/example/jedis/SemaphoreDemo.java
@@ -1,9 +1,29 @@
 package org.example.jedis;
 
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
 /**
  *
  * @author wxz
  * @date 15:37 2022/11/15
  */
 public class SemaphoreDemo {
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(3);
+        for (int i = 1; i <= 6; i++) {
+            new Thread(() -> {
+                try {
+                    semaphore.acquire();
+                    System.out.println(Thread.currentThread().getName() + "\t 抢到车位");
+                    TimeUnit.SECONDS.sleep(3);
+                    System.out.println(Thread.currentThread().getName() + "\t 停车3秒后离开车位");
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    semaphore.release();
+                }
+            }, String.valueOf(i)).start();
+        }
+    }
 }
