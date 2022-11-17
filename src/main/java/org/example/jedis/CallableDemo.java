772158593a52c1d8ea1087cac44fdbf37b2505ed
wxz <759126349@qq.com>
2022-11-17 10:30:31 +0800

    优化代码

--- src/main/java/org/example/jedis/CallableDemo.java
+++ src/main/java/org/example/jedis/CallableDemo.java
@@ -1,9 +1,31 @@
 package org.example.jedis;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+class MyThread implements Callable<Integer> {
+    @Override
+    public Integer call() throws Exception {
+        System.out.println("come in Callable");
+        return 1024;
+    }
+}
+
 /**
  *
  * @author wxz
  * @date 22:31 2022/11/16
  */
 public class CallableDemo {
+    public static void main(String[] args) {
+        FutureTask<Integer> futureTask = new FutureTask<>(new MyThread());
+        Thread t1 = new Thread(futureTask, "AAA");
+        t1.start();
+        try {
+            System.out.println(futureTask.get());
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
