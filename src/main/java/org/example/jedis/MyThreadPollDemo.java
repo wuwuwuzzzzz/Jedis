d186321832b4df208b9f6525347a363be1d14a72
wxz <759126349@qq.com>
2022-11-17 15:12:55 +0800

    优化代码

--- src/main/java/org/example/jedis/MyThreadPollDemo.java
+++ src/main/java/org/example/jedis/MyThreadPollDemo.java
@@ -1,21 +1,31 @@
 package org.example.jedis;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 /**
- *
  * @author wxz
  * @date 10:31 2022/11/17
  */
 public class MyThreadPollDemo {
     public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(5);
-
+        ExecutorService executorService = new ThreadPoolExecutor(
+                2,
+                5,
+                100L,
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(3),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.AbortPolicy());
         try {
-            for (int i = 1; i < 10; i++) {
+            for (int i = 1; i <= 8; i++) {
+                final int tmpI = i;
                 executorService.execute(() -> {
-                    System.out.println(Thread.currentThread().getName() + "\t 办理业务");
+                    System.out.println(Thread.currentThread().getName() + "号窗口，" + "服务顾客" + tmpI);
+                    try {
+                        TimeUnit.SECONDS.sleep(4);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
                 });
             }
         } catch (Exception e) {
