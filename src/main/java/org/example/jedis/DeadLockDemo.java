f2399059b46ae26a19e25381b45472f7b82e795a
wxz <759126349@qq.com>
2022-11-18 09:48:42 +0800

    优化代码

---
+++ src/main/java/org/example/jedis/DeadLockDemo.java
@@ -0,0 +1,47 @@
+package org.example.jedis;
+
+import org.omg.CORBA.TIMEOUT;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+
+class HoldLockThread implements Runnable {
+
+    private String lockA;
+
+    private String lockB;
+
+    public HoldLockThread(String lockA, String lockB) {
+        this.lockA = lockA;
+        this.lockB = lockB;
+    }
+
+    @Override
+    public void run() {
+        synchronized (lockA) {
+            System.out.println(Thread.currentThread().getName() + "\t 自己持有:" + lockA + "\t 尝试获取:" + lockB);
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            synchronized (lockB) {
+                System.out.println(Thread.currentThread().getName() + "\t 自己持有:" + lockB + "\t 尝试获取:" + lockA);
+            }
+        }
+    }
+}
+
+/**
+ *
+ * @author wxz
+ * @date 09:33 2022/11/18
+ */
+public class DeadLockDemo {
+    public static void main(String[] args) {
+        String lockA = "lockA";
+        String lockB = "lockB";
+        new Thread(new HoldLockThread(lockA, lockB), "ThreadAAA").start();
+        new Thread(new HoldLockThread(lockB, lockA), "ThreadBBB").start();
+    }
+}
