03a2ce4a72da4e942382dcdf1ce92ac16724cc1e
wxz <759126349@qq.com>
2022-11-19 16:10:27 +0800

    优化代码

--- src/main/java/org/example/jedis/SoftReferenceDemo.java
+++ src/main/java/org/example/jedis/SoftReferenceDemo.java
@@ -1,9 +1,43 @@
 package org.example.jedis;
 
+import java.lang.ref.SoftReference;
+
 /**
  *
  * @author wxz
  * @date 15:59 2022/11/19
  */
 public class SoftReferenceDemo {
+
+    public static void softRefMemoryEnough() {
+        Object o1 = new Object();
+        SoftReference<Object> softReference = new SoftReference<>(o1);
+        System.out.println(o1);
+        System.out.println(softReference.get());
+        o1 = null;
+        System.gc();
+        System.out.println(o1);
+        System.out.println(softReference.get());
+    }
+
+    public static void softRefMemoryNotEnough() {
+        Object o1 = new Object();
+        SoftReference<Object> softReference = new SoftReference<>(o1);
+        System.out.println(o1);
+        System.out.println(softReference.get());
+        o1 = null;
+        try {
+            byte[] bytes = new byte[30 * 1024 * 1024];
+        } catch (Throwable e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println(o1);
+            System.out.println(softReference.get());
+        }
+    }
+
+    public static void main(String[] args) {
+//        softRefMemoryEnough();
+        softRefMemoryNotEnough();
+    }
 }
