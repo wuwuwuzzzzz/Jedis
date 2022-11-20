cfd4ba0f2bd99a72d62a76421a8b3a816a77228d
wxz <759126349@qq.com>
2022-11-20 19:09:03 +0800

    优化代码

--- src/main/java/org/example/jedis/PhantomReferenceDemo.java
+++ src/main/java/org/example/jedis/PhantomReferenceDemo.java
@@ -1,30 +1,30 @@
 package org.example.jedis;
 
+import java.lang.ref.PhantomReference;
 import java.lang.ref.ReferenceQueue;
-import java.lang.ref.WeakReference;
 
 /**
  *
  * @author wxz
- * @date 18:20 2022/11/19
+ * @date 18:34 2022/11/19
  */
-public class ReferenceQueueDemo {
+public class PhantomReferenceDemo {
     public static void main(String[] args) throws InterruptedException {
         Object o1 = new Object();
         ReferenceQueue<Object> referenceQueue = new ReferenceQueue();
-        WeakReference<Object> weakReference = new WeakReference<>(o1, referenceQueue);
+        PhantomReference<Object> phantomReference = new PhantomReference<>(o1, referenceQueue);
 
         System.out.println(o1);
-        System.out.println(weakReference.get());
+        System.out.println(phantomReference.get());
         System.out.println(referenceQueue.poll());
 
-        System.out.println("=====================");
+        System.out.println("====================");
         o1 = null;
         System.gc();
         Thread.sleep(500);
 
         System.out.println(o1);
-        System.out.println(weakReference.get());
+        System.out.println(phantomReference.get());
         System.out.println(referenceQueue.poll());
     }
 }
